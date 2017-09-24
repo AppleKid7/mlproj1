c99ac9b14087166cabac9682014ea0a3640ed7e3
AppleKid7 <kvltd00d@gmail.com>
2017-09-23 21:20:09 -0400

    changed the experiment running script to be in better order

--- src/runner.R
+++ src/runner.R
@@ -1,10 +1,22 @@
 source('dtree.R')
 
+# DECISION TREE
+
+runtree("vehicles/vehicles.csv", 'vehicles_tree_table.png')
+tree_error("vehicles/vehicles.csv", 'vehicles_tree_error.png')
+
+runtree("wine/red_wine.csv", 'wine_tree_table.png')
+tree_error("wine/red_wine.csv",'wine_tree_error.png')
+
+# BOOSTed DECISION TREE
+
 runtree("vehicles/vehicles.csv", TRUE)
 tree_error("vehicles/vehicles.csv", 'vehicles_boost_error.png', TRUE)
 
 runtree("wine/red_wine.csv", TRUE)
 tree_error("wine/red_wine.csv", 'wine_boost_error.png', TRUE)
+
+# NEURAL NETWORK
 
 source('nn.R')
 
@@ -14,18 +26,14 @@ nn_error('vehicles/vehicles.csv', 'vehicles_nnet_error.png')
 runneuralnet('wine/red_wine.csv')
 nn_error('wine/red_wine.csv', 'wine_nnet_error.png')
 
-# DECISION TREE
-
-runtree("vehicles/vehicles.csv", 'vehicles_tree_table.png')
-tree_error("vehicles/vehicles.csv", 'vehicles_tree_error.png')
-
-runtree("wine/red_wine.csv", 'wine_tree_table.png')
-tree_error("wine/red_wine.csv",'wine_tree_error.png')
+# SUPPORT VECTOR MACHINE
 
 source('svm.R')
 
 runsvm('vehicles/vehicles.csv')
 svm_error('vehicles/vehicles.csv', 'vehicles_svm_error.png')
+
+# K-NEAREST NEIGHBORS
 
 source('knn.R')
 
