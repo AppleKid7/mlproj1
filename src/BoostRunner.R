3d2c9ace6275f479de2017de30d6a82a0e813fda
AppleKid7 <kvltd00d@gmail.com>
2017-09-23 21:00:20 -0400

    simplified thigns by adding just a single runner, began writing analysis. All algorithms are now working

--- src/BoostRunner.R
+++
@@ -1,9 +0,0 @@
-source('dtree.R')
-
-runtree("vehicles/vehicles.csv", TRUE)
-
-tree_error("vehicles/vehicles.csv", 'vehicles_boost_error.png', TRUE)
-
-runtree("wine/red_wine.csv", TRUE)
-
-tree_error("wine/red_wine.csv", 'wine_boost_error.png', TRUE)
--- src/DecisionTreeRunner.R
+++
@@ -1,9 +0,0 @@
-source('dtree.R')
-
-runtree("vehicles/vehicles.csv", 'vehicles_tree_table.png')
-
-tree_error("vehicles/vehicles.csv", 'vehicles_tree_error.png')
-
-runtree("wine/red_wine.csv", 'wine_tree_table.png')
-
-tree_error("wine/red_wine.csv",'wine_tree_error.png')
